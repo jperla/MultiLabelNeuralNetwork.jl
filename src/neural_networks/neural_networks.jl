fb8856665da6bcb359e2d9a9dcb387e9aad6a9b9
Joseph Perla <joseph@ not an email.com>
2014-03-11 19:08:42 -0400

    Fix sln_mll test so that zero! works

--- src/neural_networks/neural_networks.jl
+++ src/neural_networks/neural_networks.jl
@@ -2,11 +2,11 @@ include("abstract_neural_networks.jl")
 
 function zero!(nn::NeuralNetworkStorage)
     # Zeroes out all of the arrays in the neural network type
-    names = names(nn)
-    types = nn.types
-    for t,n in zip(types, names)
-        if isa(t, Weights)
-        fill!(nn.(n), 0)
+    fields = names(nn)
+    types = typeof(nn).types
+    for (t,f) in zip(types, fields)
+        if t <: Weights
+            fill!(nn.(f), 0)
         end
     end
 end
