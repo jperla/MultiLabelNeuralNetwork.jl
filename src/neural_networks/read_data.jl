b8e5128a0c199a3709c269647f6d1647d2ce54b8
Joseph Perla <joseph@ not an email.com>
2014-03-15 18:04:36 -0400

    Read data from a file

--- src/neural_networks/read_data.jl
+++ src/neural_networks/read_data.jl
@@ -1,38 +1,33 @@
 
 function read_data(x::String, mode::String)
-
     num_features = num_labels = 0
 
     if x == "scene"
         num_features = 294
         num_labels = 6
-    end
-
-    if x == "yeast"
+    elseif x == "yeast"
         num_features = 103
         num_labels = 14
-    end
-
-    if x == "emotions"
+    elseif x == "emotions"
         num_features = 72
         num_labels = 6
-
+    else
+        error("Unknown dataset")
     end
 
     if mode == "train"
-        suffix = "-train.csv"
+        suffix = "train"
     else
-        suffix = "-test.csv"
+        suffix = "test"
     end
 
-    path = string("data/", x, "/", x, suffix)
+    path = joinpath("data", x, string(x, "-", suffix, ".csv"))
     file = readdlm(path, ',')
 
     n = size(file, 1)
     features = file[1:n, 1:num_features]
-    labels = fil[1:n, (num_features + 1):(num_features+num_labels))
+    labels = file[1:n, (num_features + 1):(num_features + num_labels)]
 
     return (features, labels)
-
 end
 
