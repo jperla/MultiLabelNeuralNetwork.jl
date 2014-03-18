d515366faa0178745d54fbb0971dde88043eb319
Joseph Perla <joseph@ not an email.com>
2014-03-17 20:01:45 -0400

    Fix SLN_MLL Tests

--- src/NeuralNetworks.jl
+++ src/NeuralNetworks.jl
@@ -8,7 +8,7 @@ importall Base
 export SLN_MLL, SLN_MLL_Activation, SLN_MLL_Deltas, SLN_MLL_Derivatives,
        VectorOrSubArrayVector,
        forward_propagate!, calculate_label_probabilities!,
-       back_propagate!, sigmoid_prime, square_loss, log_loss, zero_one_loss
+       back_propagate!, sigmoid_prime, square_loss, log_loss, zero_one_loss,
        log_loss_prime, sigmoid, relu,
        zero!, gradientm,
        top_features, top_weights,
